fa3906a1ace1d485b79d0fbb701be8658ad3ce13
imrole <51855981+imrole@users.noreply.github.com>
2020-10-29 11:03:13 +0800

    Add files via upload

---
+++ JDBook.IBLL/IUserBLL.cs
@@ -0,0 +1,28 @@
+﻿using JDBook.Model.Request;
+using JDBook.Model.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JDBook.IBLL
+{
+    /// <summary>
+    /// 用户业务逻辑接口
+    /// </summary>
+    public interface IUserBLL
+    {
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="loginModel">登录模型</param>
+        /// <returns></returns>
+        public ResponseModel Login(LoginModel loginModel);
+
+        /// <summary>
+        /// 用户注册
+        /// </summary>
+        /// <param name="registerModel">注册模型</param>
+        /// <returns></returns>
+        public ResponseModel Register(RegisterModel registerModel);
+    }
+}
---
+++ JDBook.Server/BookHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JDBook.Server
+{
+    class BookHelper
+    {
+    }
+}
